59bee1ebb1ecfd8d944b799c4d6cae6b70aae4b9
Teguh Santoso <101353193+tegohsx@users.noreply.github.com>
2022-03-12 14:11:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "DB_URL": process.env.DB_URL || "mongodb+srv://username:password@klaster0.xxxx.mongodb.net/dbname?retryWrites=true&w=majority",
+    "BOT_TOKEN": process.env.BOT_TOKEN || "1234567890:abcdefghijk-kjhgfdsasd-qwertyuiop",
+    "BOT_NAME":  process.env.BOT_NAME || "AnonTalk"
+}
